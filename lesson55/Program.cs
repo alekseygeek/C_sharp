deb0779f262580ffe9e6ba51ef42a8164bee041c
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-14 19:28:47 +0300

    создали Машку:

---
+++ lesson55/Program.cs
@@ -0,0 +1,13 @@
+﻿// See
+Console.WriteLine("имя пользовалеля");
+string username = Console.ReadLine();
+if(username.ToLower() =="masha")
+
+{
+    Console.WriteLine("ура ,это masha");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine();
+}
