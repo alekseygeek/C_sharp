1b156625dc8a3ac1c9d10dc85d297dee3fb55c7f
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-15 09:17:38 +0300

    программа рандомные числа

---
+++ lesson66/Program.cs
@@ -0,0 +1,7 @@
+﻿// сумма чисел
+int numberA = new Random().Next(1, 100);// 1,2,3...100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
